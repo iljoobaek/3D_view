1b3bc86a6d20d20881289bf1c05e0e9df06ac6f8
Akshit Kanda <akanda@andrew.cmu.edu>
2018-11-11 17:17:33 -0800

    normalL_narrowR-front_algo modified

--- Calibration/mainProcess/Final_calibration files/asymmetric/normalL_narrowR/front_algorithm.m
+++ Calibration/mainProcess/Final_calibration files/asymmetric/normalL_narrowR/front_algorithm.m
@@ -1,19 +1,33 @@
+% This code implements the Thin Plate Spline algorithm for mapping 3D mesh
+% vertices (vt values in .OBJ files) to corresponding 2D pixel coordinates
+% (ultimately v values in .OBJ file)
+% Specifically, the following program maps vt - v values for front view
+% of assymetric narrow right mesh
+
+% We take a few vertices which approximate the shape of the mesh as an 
+% input to the algorithm
+
+% Taking evenly spaced vertices from top row of mesh
 x_mesh_back_u = [-2.631, 0, 2.151];
 y_mesh_back_u = [4.343, 4.343, 4.343];
 z_mesh_back_u = [0.9659, 0.9659, 0.9659];
 
+% Taking evenly spaced vertices from right boundary of mesh
 x_mesh_back_r = [2.04, 1.586, 1.215, 0.72];
 y_mesh_back_r = [4.16, 3.332, 2.654, 1.75];
 z_mesh_back_r = [0, 0, 0, 0];
 
+% Taking evenly spaced vertices from bottom row of mesh
 x_mesh_back_b = [0.54, 0.36, 0.18, 0, -0.18, -0.36, -0.54, -0.72];
 y_mesh_back_b = [1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75];
 z_mesh_back_b = [0, 0, 0, 0, 0, 0, 0, 0];
 
+% Taking evenly spaced vertices from left boundary of mesh
 x_mesh_back_l = [-1.337, -1.954, -2.52];
 y_mesh_back_l = [2.576, 3.403, 4.16];
 z_mesh_back_l = [0, 0, 0];
 
+% Forming a single array for each of x, y and z
 x_mesh_back_half1 = horzcat(x_mesh_back_u, x_mesh_back_r);
 y_mesh_back_half1 = horzcat(y_mesh_back_u, y_mesh_back_r);
 z_mesh_back_half1 = horzcat(z_mesh_back_u, z_mesh_back_r);
@@ -22,6 +36,7 @@ x_mesh_back_half2 = horzcat(x_mesh_back_b, x_mesh_back_l);
 y_mesh_back_half2 = horzcat(y_mesh_back_b, y_mesh_back_l);
 z_mesh_back_half2 = horzcat(z_mesh_back_b, z_mesh_back_l);
 
+% Taking evenly spaced vertices from the middle of mesh
 x_mid = [-1.89, -1.26, -0.63, 0, 0.51, 1.02, 1.53, 0, 0];
 y_mid = [4.16, 4.16, 4.16, 4.16, 4.16, 4.16, 4.16, 3.403, 2.576];
 z_mid = [0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -33,6 +48,7 @@ y_mesh_back = horzcat(y_mesh_back, y_mid);
 z_mesh_back = horzcat(z_mesh_back_half1, z_mesh_back_half2);
 z_mesh_back = horzcat(z_mesh_back, z_mid);
 
+% Points clicked on image for 2D input
 x = x_clicked(1:27)';
 y = y_clicked(1:27)';
 
@@ -52,6 +68,8 @@ for i = 1:points_clicked
     y_column (i, 1) = y_mesh_back(i);
     z_column (i, 1) = z_mesh_back(i);
 end
+
+% Form 4 sub matrices
 part1_1_in = horzcat(all_ones, x_column);
 part1_1 = horzcat(part1_1_in, y_column);
 part1_1 = horzcat(part1_1, z_column);
@@ -69,11 +87,13 @@ for i = 1:points_clicked
         end
     end
 end
-    
+
+% Combine all the sub matrices to form a single transformation matrix
 T_upper = horzcat(part1_1, part1_2);
 T_lower = horzcat(part2_1, part2_2);
 T = vertcat(T_upper, T_lower);
-      
+
+% calculate the inversion vector 
 c_x_back = T \ xp_back;
 c_y_back = T \ yp_back;
 
@@ -87,12 +107,16 @@ x_new = v(1, n);
 y_new = v(2, n);
 z_new = v(3, n);
 
+% store all the vertices (v) of the mesh in a matrix
 v_new = vertcat(x_new, y_new);
 v_new = vertcat(v_new, z_new);
 
+% form two vectors for deriving vt values for corresponding v values
 x_bar_back = zeros(2145, 1);
 y_bar_back = zeros(2145, 1);
 
+% for each vertex, get corresponding value in 2D using the above derived 
+% c_x_back and c_y_back
 count = 0;
 for i = 1:2145
     count = count + 1;
@@ -124,10 +148,7 @@ for i = 1:2145
         y_bar_back(i) = max(y_bar_back);
     end
 end
-% for live stream
-% x_bar_new_norm = abs(x_bar_back)./1920;
-% y_bar_new_norm = abs(1 - (y_bar_back)./1218);
 
-% for recorded
+% to get final vt values, the values need to be bw 0 and 1 on both axes
 x_bar_new_norm = abs(1 - (x_bar_back)./1920);
 y_bar_new_norm = abs((y_bar_back)./1208);
